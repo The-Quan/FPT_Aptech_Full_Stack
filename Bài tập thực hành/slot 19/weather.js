78f8f1b86eb0eddc06f4f86ec8a5cd844644fb36
The-Quan <145833348+The-Quan@users.noreply.github.com>
2023-10-20 18:57:26 +0700

    update

--- Bài tập thực hành/slot 19/weather.js
+++ Bài tập thực hành/slot 19/weather.js
@@ -1,4 +1,4 @@
-$.get ("https://api.openweathermap.org/data/2.5/weather?q=hanoi&appid=96381a872b1b405c5bf83b2ed63d9561&mode=json&units=metric",
+$.get ("https://api.openweathermap.org/data/2.5/weather?q=Bắc kạn&appid=96381a872b1b405c5bf83b2ed63d9561&mode=json&units=metric",
     function (api) {
    $('.temp').html('Local Temp: '+ api.main.temp + 'C');
     $('.city').html('City : '+ api.name);
