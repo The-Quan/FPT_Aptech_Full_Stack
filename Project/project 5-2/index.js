8742a214b51ccf391829947b38043108d6a5b827
The-Quan <145833348+The-Quan@users.noreply.github.com>
2023-11-14 16:11:56 +0700

    up

---
+++ Project/project 5-2/index.js
@@ -0,0 +1,14 @@
+
+        const marker = document.querySelector('#marker');
+        const item = document.querySelectorAll('nav a');
+
+
+        function indicator(e) {
+            marker.style.left = e.offsetLeft +'px';
+            marker.style.width = e.offsetWidth + 'px';
+        }
+        item.forEach(link => {
+            link.addEventListener('click', (e) => {
+                indicator(e.target);
+            });
+        });
