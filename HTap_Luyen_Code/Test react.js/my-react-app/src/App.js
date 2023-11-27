90bb2158311cca247b781d52f8cc146ae6dd828d
The-Quan <quanthe2202@gmail.com>
2023-11-27 21:54:41 +0700

    up

--- HTap_Luyen_Code/Test react.js/my-react-app/src/App.js
+++ HTap_Luyen_Code/Test react.js/my-react-app/src/App.js
@@ -1,9 +1,6 @@
-import logo from './logo.svg';
-import './App.css';
-
 function App() {
   return (
-    <div className="App">
+    <div className="app">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
