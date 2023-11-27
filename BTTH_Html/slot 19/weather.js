012d5890d38682d0e0e06530dd769e8006cdb4a9
The-Quan <145833348+The-Quan@users.noreply.github.com>
2023-11-27 16:44:26 +0700

    up

---
+++ BTTH_Html/slot 19/weather.js
@@ -0,0 +1,7 @@
+$.get ("https://api.openweathermap.org/data/2.5/weather?q=Bắc kạn&appid=96381a872b1b405c5bf83b2ed63d9561&mode=json&units=metric",
+    function (api) {
+   $('.temp').html('Local Temp: '+ api.main.temp + 'C');
+    $('.city').html('City : '+ api.name);
+    $('.description').html('Description: ' + api.weather[0].description);
+    $('.icon img').attr('src','http://openweathermap.org/img/wn/' + api.weather[0].icon +'.png')
+});
