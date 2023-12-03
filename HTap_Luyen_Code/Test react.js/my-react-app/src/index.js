da368aef5ffba6e05cb795e21ad10bdf54db95af
The-Quan <quanthe2202@gmail.com>
2023-12-03 21:26:29 +0700

    up

--- HTap_Luyen_Code/Test react.js/my-react-app/src/index.js
+++ HTap_Luyen_Code/Test react.js/my-react-app/src/index.js
@@ -1,15 +1,10 @@
-import React from 'react';
+import React, { StrictMode } from 'react';
 import ReactDOM from 'react-dom/client';
-import './index.css';
 import App from './App';
-import reportWebVitals from './reportWebVitals';
-import Car from './Luyen_Code/bai1';
-import App11 from './Luyen_Code/bai1';
-import Football from './Luyen_Code/bai2';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <StrictMode>
-    <Football />
+    <App />
   </StrictMode>
 );
