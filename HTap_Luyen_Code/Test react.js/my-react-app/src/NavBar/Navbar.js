339354e19530ff40d3b61016ad24d7e883d5c1c1
The-Quan <quanthe2202@gmail.com>
2023-12-03 23:49:49 +0700

    up

---
+++ HTap_Luyen_Code/Test react.js/my-react-app/src/NavBar/Navbar.js
@@ -0,0 +1,8 @@
+function Navbar(){
+    return(
+        <div>
+            <h3>this is my project</h3>
+        </div>
+    );
+}
+export default Navbar;
--- HTap_Luyen_Code/Test react.js/my-react-app/src/index.js
+++ HTap_Luyen_Code/Test react.js/my-react-app/src/index.js
@@ -1,10 +1,18 @@
 import React, { StrictMode } from 'react';
 import ReactDOM from 'react-dom/client';
 import App from './App';
+import Football from './Project/bai2';
+import myElement from './Project/bai3';
+import App11 from './Project/bai1';
+import AnotherComponent from './Project/bai3';
+import Car from './Project/bai4';
+import Garage from './Project/bai5';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
-  <StrictMode>
-    <App />
-  </StrictMode>
+
+    <StrictMode>
+        <Garage />
+    </StrictMode>
+
 );
