193bfbfe5f7570bf3914d1af76a2d6a136e549d5
The-Quan <quanthe2202@gmail.com>
2023-12-04 23:17:46 +0700

    up

---
+++ HTap_Luyen_Code/Test react.js/my-react-app/src/Baitap/bai7.js
@@ -0,0 +1,15 @@
+import React from "react";
+import ReactDOM from "react";
+
+function MyForm(){
+    return (
+        <from>
+            <label>
+                Enter your name:
+                <input type="text"></input>
+            </label>
+        </from>
+    );
+};
+
+export default MyForm;
