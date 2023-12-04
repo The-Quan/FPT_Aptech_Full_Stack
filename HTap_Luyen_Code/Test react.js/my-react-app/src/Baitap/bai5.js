0df929281ba9cfd6d891ef78819dc9ad431065c7
The-Quan <quanthe2202@gmail.com>
2023-12-04 23:17:27 +0700

    up

---
+++ HTap_Luyen_Code/Test react.js/my-react-app/src/Baitap/bai5.js
@@ -0,0 +1,16 @@
+
+function Car (){
+    return <h2>I am a car</h2>
+}
+
+function Garage1(){
+    return (
+    <>
+    <h1>
+        who lives in my Garage
+    </h1>
+    <car />
+    </>
+    );
+}
+export default Garage1;
