0549a8886f96dc6bbd1b93e5f575c5d2386a8a93
The-Quan <quanthe2202@gmail.com>
2023-12-04 23:17:54 +0700

    up

--- HTap_Luyen_Code/Test react.js/my-react-app/src/index.js
+++ HTap_Luyen_Code/Test react.js/my-react-app/src/index.js
@@ -1,18 +1,20 @@
 import React, { StrictMode } from 'react';
 import ReactDOM from 'react-dom/client';
 import App from './App';
-import Football from './Project/bai2';
-import myElement from './Project/bai3';
-import App11 from './Project/bai1';
-import AnotherComponent from './Project/bai3';
-import Car from './Project/bai4';
-import Garage from './Project/bai6';
+import Football from './Baitap/bai2';
+import myElement from './Baitap/bai3';
+import App11 from './Baitap/bai1';
+import AnotherComponent from './Baitap/bai3';
+import Car from './Baitap/bai4';
+import Garage from './Baitap/bai6';
+import MyForm from './Baitap/bai7';
+
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
 
     <StrictMode>
-        <Garage />
+        <MyForm />
     </StrictMode>
 
 );
