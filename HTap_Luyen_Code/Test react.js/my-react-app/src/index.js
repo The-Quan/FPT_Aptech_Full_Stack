7451efb9a72e4010ffdd8da900d1d4274f59dd75
The-Quan <quanthe2202@gmail.com>
2023-12-07 14:26:02 +0700

    up

--- HTap_Luyen_Code/Test react.js/my-react-app/src/index.js
+++ HTap_Luyen_Code/Test react.js/my-react-app/src/index.js
@@ -1,20 +1,17 @@
-import React, { StrictMode } from 'react';
+import React from 'react';
 import ReactDOM from 'react-dom/client';
+import './index.css';
 import App from './App';
-import Football from './Baitap/bai2';
-import myElement from './Baitap/bai3';
-import App11 from './Baitap/bai1';
-import AnotherComponent from './Baitap/bai3';
-import Car from './Baitap/bai4';
-import Garage from './Baitap/bai6';
-import MyForm from './Baitap/bai7';
-
+import reportWebVitals from './reportWebVitals';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
-
-    <StrictMode>
-        <MyForm />
-    </StrictMode>
-
+  <React.StrictMode>
+    <App />
+  </React.StrictMode>
 );
+
+// If you want to start measuring performance in your app, pass a function
+// to log results (for example: reportWebVitals(console.log))
+// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
+reportWebVitals();
