174b07e0e7a54aa88c9df7f676449c3cc9e5637c
The-Quan <quanthe2202@gmail.com>
2023-12-08 23:23:53 +0700

    up

--- HTap_Luyen_Code/Test react.js/my-react-app/src/index.js
+++ HTap_Luyen_Code/Test react.js/my-react-app/src/index.js
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { StrictMode } from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App1';
@@ -12,7 +12,12 @@ import myElement5 from './Baitap/bai16';
 import myElement6 from './Baitap/bai17';
 import myElement7 from './Baitap/bai18';
 import myElement8 from './Baitap/bai18';
+import Content from './bai_thi';
 
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
-root.render(myElement8);
+root.render(
+    <StrictMode>
+        <Content />
+    </StrictMode>
+);
