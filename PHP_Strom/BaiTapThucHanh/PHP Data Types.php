b9c55d571b2f9c9e9e271dff9d9e00b9dfe24962
The-Quan <145833348+The-Quan@users.noreply.github.com>
2024-01-08 22:10:15 +0700

    up

---
+++ PHP_Strom/BaiTapThucHanh/PHP Data Types.php
@@ -0,0 +1,14 @@
+<?php
+$x = 5;
+var_dump($x); // Var_dump dung de lay kieu du lieu
+/* các kieu du lieu
+* String
+* Integer
+* Float (floating point numbers - also called double)
+* Boolean
+* Array
+* Object
+* NULL
+* Resource
+*/
+
---
+++ PHP_Strom/BaiTapThucHanh/PHP number.php
@@ -0,0 +1,13 @@
+<?php
+/* Có ba loại số chính trong PHP:
+* Integer
+* Float
+* Number Strings
+Ngoài ra, PHP còn có thêm 2 kiểu dữ liệu dùng cho số:
+* Infinity
+* NaN
+*/
+
+$a = 5;
+$b = 5.34;
+$c = "25";
