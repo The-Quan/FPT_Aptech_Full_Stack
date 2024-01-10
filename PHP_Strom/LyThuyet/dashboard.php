1066ac302ddc59cb2115a377c913aa70355f750e
The-Quan <145833348+The-Quan@users.noreply.github.com>
2024-01-10 21:59:01 +0700

    up

--- PHP_Strom/LyThuyet/dashboard.php
+++
@@ -1,9 +0,0 @@
-<?php
-//echo "welcome to admin zone";
-session_start();
-// kiểm tra
-if(!isset($_SESSION["username"])){
-    header("Location: index.html");
-    exit();
-}
-  echo "welcome to admin 1";
