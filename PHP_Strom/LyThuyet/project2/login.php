0c0093eabd1126f4dad4543da19fca8f75891178
The-Quan <145833348+The-Quan@users.noreply.github.com>
2024-01-10 21:59:35 +0700

    up

---
+++ PHP_Strom/LyThuyet/project2/login.php
@@ -0,0 +1,29 @@
+<?php
+$host ="localhost:3306";
+$username="root";
+$password ="";
+$database = "quanfpt";
+// kết nối csdl
+$conn = new mysqli($host, $username, $password, $database);
+//kiểm tra kết nối có thành công hay không
+if($conn->connect_error){
+    die("kết nối đến csdl ko thành công".$conn->connect_error);
+}
+// xử lý phần login
+$username =$_POST["username"];
+$password =$_POST["password"];
+
+// thực hiện truy vấn thông tin đăng nhập
+
+$query = "select * from account whthực hiện truy vẫn đến cơ sở dữ liệu server
+ere username = 'username' and password='password'";
+$result = $conn->query($query); //
+if($result->num_rows>0){
+    echo "đăng nhập thành công";
+    header("location:dashboard.php"); //điều hướng sang dashboard.php nếu login thành công.
+}else{
+    echo "đăng nhập ko thành công. vui lòng thử lại";
+}
+//đóng kết nối sau khi đã thực hiện xong
+$conn->close();
+
