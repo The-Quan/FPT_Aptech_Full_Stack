b071e59cc810cbaf1405904d2219a238c102dce9
The-Quan <145833348+The-Quan@users.noreply.github.com>
2024-01-13 23:09:00 +0700

    up

---
+++ PHP_Strom/BaiTapThucHanh/baiTestPHP/index.php
@@ -0,0 +1,26 @@
+<?php
+include "condb.php";
+?>
+<!doctype html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <title>Them,Sua,Xoa</title>
+</head>
+<body>
+<table border="1" align="center">
+    <tr>
+        <th>UserName</th>
+        <th>Email</th>
+        <th>Address</th>
+        <th>Them</th>
+    </tr>
+    <tr>
+        <td>Peter</td>
+        <td>peter@gamil.com</td>
+        <td>13 london</td>
+        <td>Sua | Xoa</td>
+    </tr>
+</table>
+</body>
+</html>
