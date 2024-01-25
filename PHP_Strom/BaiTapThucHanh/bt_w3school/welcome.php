2c68ebd4d9d60e3a9175732723e01422ada6382d
The-Quan <145833348+The-Quan@users.noreply.github.com>
2024-01-25 23:05:00 +0700

    up

---
+++ PHP_Strom/BaiTapThucHanh/bt_w3school/welcome.php
@@ -0,0 +1,11 @@
+<!doctype html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <title>welcome</title>
+</head>
+<body>
+welcome: <?php echo $_POST["name"]?><br>
+email: <?php echo $_POST["email"]?>
+</body>
+</html>
