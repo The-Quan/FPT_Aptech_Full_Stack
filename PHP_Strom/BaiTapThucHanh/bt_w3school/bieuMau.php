1ff5c6adbce3bad1d965e1e5755e6599d6c85cc4
The-Quan <145833348+The-Quan@users.noreply.github.com>
2024-01-25 23:04:46 +0700

    up

---
+++ PHP_Strom/BaiTapThucHanh/bt_w3school/bieuMau.php
@@ -0,0 +1,14 @@
+<!doctype html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <title>login</title>
+</head>
+<body>
+<form action="welcome.php" method="POST">
+    name: <input type="text" name="name"><br>
+    email: <input type="text" name="email"><br>
+    <input type="submit">
+</form>
+</body>
+</html>
