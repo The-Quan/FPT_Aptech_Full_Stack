8ea8378e68e62b6936d38f5092ee30c287a62625
The-Quan <145833348+The-Quan@users.noreply.github.com>
2024-05-30 15:39:55 +0700

    up

--- java/BaiThiThucHanhJson/src/baithithuchanh/WriteJson.java
+++ java/BaiThiThucHanhJson/src/baithithuchanh/WriteJson.java
@@ -2,8 +2,6 @@ package baithithuchanh;
 
 import com.google.gson.Gson;
 import com.google.gson.GsonBuilder;
-
-import javax.security.auth.Subject;
 import java.io.FileWriter;
 import java.io.IOException;
 import java.util.ArrayList;
@@ -12,15 +10,15 @@ import java.util.List;
 public class WriteJson {
     public static void main(String[] args) {
         // Tạo danh sách sinh viên
-        List<baithithuchanh.Student> students = new ArrayList<>();
+        List<Student> students = new ArrayList<>();
 
         // Thêm sinh viên vào danh sách
-        baithithuchanh.Student student1 = new baithithuchanh.Student("1", "Truong Gia Binh");
+        Student student1 = new Student("1", "Truong Gia Binh");
         student1.addSubject(new Subject("Math", new int[]{10, 9, 8}));
         student1.addSubject(new Subject("Physics", new int[]{7, 8, 9}));
         students.add(student1);
 
-        baithithuchanh.Student student2 = new baithithuchanh.Student("2", "Le Truong Tung");
+        Student student2 = new Student("2", "Le Truong Tung");
         student2.addSubject(new Subject("Chemistry", new int[]{8, 9, 10}));
         student2.addSubject(new Subject("Biology", new int[]{7, 7, 8}));
         students.add(student2);
