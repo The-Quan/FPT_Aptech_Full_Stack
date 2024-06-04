37cb09ce766656c9a6e98c01dfdd1d4dc7157484
The-Quan <145833348+The-Quan@users.noreply.github.com>
2024-06-04 19:29:08 +0700

    up

---
+++ C#/.Net/Lab2/Ex1/Program.cs
@@ -0,0 +1,44 @@
+﻿using System;
+class ArrayClass
+{
+    static void Main(string[] args)
+    {
+        int[] Arr = new int[12] { 29, 82, 42, 46, 54, 65, 50, 42, 5, 94, 19, 34 };
+
+        Console.WriteLine("The first occurrence of 42 is at index " + Array.IndexOf(Arr, 42));
+
+        Console.WriteLine("The last occurrence of 42 is at index " + Array.LastIndexOf(Arr, 42));
+        int x = 0;
+        while ((x = Array.IndexOf(Arr, 42, x)) >= 0)
+        {
+            Console.WriteLine("42 found at index " + x);
+            ++x;
+        }
+        x = Arr.Length - 1;
+        while ((x = Array.LastIndexOf(Arr, 42, x)) >= 0)
+        {
+            Console.WriteLine("42 found at index " + x);
+            --x;
+        }
+
+
+        Console.WriteLine("Array that before sorted");
+        for (int i = 0; i < Arr.Length; i++)
+        {
+            Console.WriteLine("{0} :      {1}", i + 1, Arr[i]);
+        }
+        Array.Sort(Arr);
+        Console.WriteLine("Array that after sorted");
+        for (int i = 0; i < Arr.Length; i++)
+        {
+            Console.WriteLine("{0} :      {1}", i + 1, Arr[i]);
+        }
+        Array.Reverse(Arr);
+        Console.WriteLine("Array that after reserse");
+        for (int i = 0; i < Arr.Length; i++)
+        {
+            Console.WriteLine("{0} :      {1}", i + 1, Arr[i]);
+        }
+        Console.ReadLine();
+    }
+}
