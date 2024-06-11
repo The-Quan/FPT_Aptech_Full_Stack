e9858dea78348a254b0af24069f429cc874b6837
The-Quan <145833348+The-Quan@users.noreply.github.com>
2024-06-11 15:26:34 +0700

    up

--- C#/.Net/Lab5/Ex1/obj/Debug/net8.0/Ex1.AssemblyInfo.cs
+++ C#/.Net/Lab5/Ex1/obj/Debug/net8.0/Ex1.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Ex1")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+e0ff5cefb9044fb950887576d1e1fc6e7e8193f4")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+dea2303ac77c2e7c47b409ff77da73ccddb4ebd9")]
 [assembly: System.Reflection.AssemblyProductAttribute("Ex1")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Ex1")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
